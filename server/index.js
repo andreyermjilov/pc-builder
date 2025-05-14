874de5c08e5edbfdac3c9d871b5a3b043f87b7c9
andreyermjilov <andrey.ermilov.86@gmail.com>
2025-05-14 20:21:11 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-    const express = require('express');
+const express = require('express');
 const { google } = require('googleapis');
 const cors = require('cors');
 const dotenv = require('dotenv');
