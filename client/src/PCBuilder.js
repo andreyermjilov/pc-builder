33c92786e1595dfed4a799bc1bc0cd95e9761287
andreyermjilov <andrey.ermilov.86@gmail.com>
2025-05-15 01:56:08 +0500

    Update PCBuilder.js

--- client/src/PCBuilder.js
+++ client/src/PCBuilder.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import './PCBuilder.css';
 
 const PCBuilder = () => {
   const [componentsByCategory, setComponentsByCategory] = useState({});
